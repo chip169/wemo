6ce858917a59c1c047cf53b2ca80f1affb27343f
kim xuan hieu <hieukxhe186118@fpt.edu.vn>
2026-06-19 21:47:06 +0700

    Add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
+);
